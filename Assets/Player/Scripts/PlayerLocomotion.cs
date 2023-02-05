63143b8e2aa78d237bdf92ee6da6858c04fba356
JonnesusS <bi24fz@student.sunderland.ac.uk>
2023-02-05 19:56:24 +0000

    V0.0.2

--- Assets/Player/Scripts/PlayerLocomotion.cs
+++ Assets/Player/Scripts/PlayerLocomotion.cs
@@ -22,15 +22,30 @@ public class PlayerLocomotion : MonoBehaviour
     {
         Cursor.visible = false;
         characterController = GetComponent<CharacterController>();
-        //SetCurrentCamera();
+        SetCurrentCamera();
     }
 
     void Update()
     {
         Locomotion();
+        RotateAndLook();
+        PerspectiveCheck();
+    }
 
-        //RotateAndLook();
-        //PerspectiveCheck();
+    void SetCurrentCamera()
+    {
+        SwitchPerspective switchPerspective = GetComponent<SwitchPerspective>();
+        if (switchPerspective.GetPerspective() == SwitchPerspective.Perspective.First)
+        {
+            playerContainer = gameObject.transform.Find("Container1P");
+            cameraContainer = playerContainer.transform.Find("Camera1PContainer");
+        }
+        else
+        {
+            playerContainer = gameObject.transform.Find("Container3P");
+            cameraContainer = playerContainer.transform.Find("Camera3PContainer");
+        }
+
     }
 
     void Locomotion()
@@ -40,11 +55,58 @@ public class PlayerLocomotion : MonoBehaviour
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
-
-            //Todo Jumping/Crouching
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+            if (Input.GetKey(KeyCode.C))
+            {
+                characterController.height = 0.65f;
+                characterController.center = new Vector3(0f, 0.5f, 0f);
+            }
+            else //if (Input.GetKeyUp(KeyCode.C))
+            {
+                characterController.height = 2f;
+                characterController.center = new Vector3(0f, 1f, 0f);
+            }
         }
 
         moveDirection.y -= gravity * Time.deltaTime;
         characterController.Move(moveDirection * Time.deltaTime);
+
+    }
+
+    void RotateAndLook()
+    {
+        rotateX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotateY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        rotateY = Mathf.Clamp(rotateY, lookUpClamp, lookDownClamp);
+
+        transform.Rotate(0f, rotateX, 0f);
+
+        cameraContainer.transform.localRotation = Quaternion.Euler(rotateY, 0f, 0f);
+    }
+
+    void PerspectiveCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SwitchPerspective switchPerspective = GetComponent<SwitchPerspective>();
+
+            if (switchPerspective != null)
+            {
+                if (switchPerspective.GetPerspective() == SwitchPerspective.Perspective.First)
+                {
+                    switchPerspective.SetPerspective(SwitchPerspective.Perspective.Third);
+                }
+                else
+                {
+                    switchPerspective.SetPerspective(SwitchPerspective.Perspective.First);
+                }
+
+                SetCurrentCamera();
+            }
+        }
     }
 }
